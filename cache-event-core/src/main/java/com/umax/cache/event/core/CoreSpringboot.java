367935d892f4db9da315203b4489d8c9262061c4
wangyb <1501987783@qq.com>
2023-02-05 17:36:54 +0800

    code clean

--- cache-event-core/src/main/java/com/umax/cache/event/core/CoreSpringboot.java
+++ cache-event-core/src/main/java/com/umax/cache/event/core/CoreSpringboot.java
@@ -1,26 +1,26 @@
-package com.umax.cache.event.core;
-
-import com.umax.cache.event.core.annotations.EnableCacheEvent;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-/**
- * @author wangyingbo
- * @date 2023-02-02 9:51
- **/
-@ComponentScan(basePackages = "com.umax.cache.event")
-@EnableCacheEvent
-@EnableAspectJAutoProxy
-@EnableCaching
-@SpringBootApplication
-public class CoreSpringboot {
-
-    public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(CoreSpringboot.class, args);
-    }
-
-}
+//package com.umax.cache.event.core;
+//
+//import com.umax.cache.event.core.annotations.EnableWebCacheEvent;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.context.ConfigurableApplicationContext;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.EnableAspectJAutoProxy;
+//
+///**
+// * @author wangyingbo
+// * @date 2023-02-02 9:51
+// **/
+//@ComponentScan(basePackages = "com.umax.cache.event")
+//@EnableWebCacheEvent
+//@EnableAspectJAutoProxy
+//@EnableCaching
+//@SpringBootApplication
+//public class CoreSpringboot {
+//
+//    public static void main(String[] args) {
+//        ConfigurableApplicationContext applicationContext = SpringApplication.run(CoreSpringboot.class, args);
+//    }
+//
+//}
--- cache-event-core/src/test/java/com/umax/cache/event/core/CoreSpringbootTests.java
+++ cache-event-core/src/test/java/com/umax/cache/event/core/CoreSpringbootTests.java
@@ -1,10 +1,14 @@
 package com.umax.cache.event.core;
 
+import com.umax.cache.event.core.annotations.EnableWebCacheEvent;
 import com.umax.cache.event.core.bean.CacheBean;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.stream.Stream;
@@ -14,7 +18,13 @@ import java.util.stream.Stream;
  * @date 2023-02-02 9:51
  **/
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = CoreSpringbootTests.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = {CoreSpringbootTests.class})
+@ComponentScan(basePackages = "com.umax.cache.event")
+@EnableWebCacheEvent
+@EnableAspectJAutoProxy
+@EnableCaching
 public class CoreSpringbootTests {
 
     @Autowired
